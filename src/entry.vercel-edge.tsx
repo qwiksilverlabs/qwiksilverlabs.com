da6b6de2972cebb24714101f5bd2f43d1127cd08
Toby Nguyen <tobynguyen27@proton.me>
2026-06-15 13:59:39 +0700

    chore: migrate to Vite+

--- src/entry.vercel-edge.tsx
+++ src/entry.vercel-edge.tsx
@@ -7,15 +7,12 @@
  * - https://qwik.dev/docs/deployments/vercel-edge/
  *
  */
-import {
-  createQwikCity,
-  type PlatformVercel,
-} from "@builder.io/qwik-city/middleware/vercel-edge";
-import qwikCityPlan from "@qwik-city-plan";
-import render from "./entry.ssr";
+import { createQwikCity, type PlatformVercel } from '@builder.io/qwik-city/middleware/vercel-edge';
+import qwikCityPlan from '@qwik-city-plan';
+import render from './entry.ssr';
 
 declare global {
-  type QwikCityPlatform = PlatformVercel;
+	type QwikCityPlatform = PlatformVercel;
 }
 
 export default createQwikCity({ render, qwikCityPlan });
